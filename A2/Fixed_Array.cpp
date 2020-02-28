ebc61caf34ccf810eed63cf1f647cf4600a63921
edtorrez <etorrez2017@gmail.com>
2020-02-28 10:24:50 -0500

    removed unnecessary methods

--- A2/Fixed_Array.cpp
+++ A2/Fixed_Array.cpp
@@ -25,17 +25,6 @@ Fixed_Array <T, N>::Fixed_Array (const Fixed_Array <T, N> & arr):
 // Fixed_Array
 //
 template <typename T, size_t N>
-template <size_t M>
-Fixed_Array <T, N>::Fixed_Array (const Fixed_Array <T, M> & arr):
-    Array<T>{ N }
-{
-    *this = arr;
-}
-
-//
-// Fixed_Array
-//
-template <typename T, size_t N>
 Fixed_Array <T, N>::Fixed_Array (T fill):
     Array<T>{ N, fill }
 {}
@@ -51,35 +40,11 @@ Fixed_Array <T, N>::~Fixed_Array (void)
 // operator =
 //
 template <typename T, size_t N>
-const Fixed_Array <T, N> & Fixed_Array <T, N>::operator = (const Fixed_Array <T, N> & rhs)
+const Fixed_Array <T, N> & Fixed_Array <T, N>::operator = (const Fixed_Array <T, N> & rhs):
 {
     if( rhs != *this ) // check self-comparison
     {
         for( int i = 0; i < N; i++ ) // copy rhs to lhs
-        {
-            Fixed_Array<T,N>::set( i, rhs.get(i) );
-        }
-    }
-    
-    return *this;
-}
-
-//
-// operator =
-//
-template <typename T, size_t N>
-template <size_t M>
-const Fixed_Array <T, N> & Fixed_Array <T, N>::operator = (const Fixed_Array <T, M> & rhs)
-{
-    if( rhs != *this ) // check self-comparison
-    {
-        size_t smallestSize = N; // find smallest array size
-        if( rhs.size() < N )
-        {
-            smallestSize = rhs.size();
-        }
-
-        for( int i = 0; i < smallestSize; i++ ) // copy rhs to lhs
         {
             Fixed_Array<T,N>::set( i, rhs.get(i) );
         }
--- A2/Fixed_Array.h
+++ A2/Fixed_Array.h
@@ -32,15 +32,6 @@ public:
   Fixed_Array (const Fixed_Array <T, N> & arr);
 	
   /**
-   * Initializing constructor. The source array can be of any size
-   * as long as they are of the same type.
-   *
-   * @param[in]      arr        Source array
-   */
-  template <size_t M>
-  Fixed_Array (const Fixed_Array <T, M> & arr);
-
-  /**
    * Initializing constructor. Fills the contents of the 
    * array with the specified \a fill value.
    *
@@ -57,14 +48,6 @@ public:
    * @param[in]      rhs        Right-hand side of operator.
    */
   const Fixed_Array & operator = (const Fixed_Array <T, N> & rhs);
-
-  /**
-   * Assignment operator
-   *
-   * @param[in]      rhs        Right-hand side of operator.
-   */
-  template <size_t M>
-  const Fixed_Array & operator = (const Fixed_Array <T, M> & rhs);
 };
 
 // include the inline files
